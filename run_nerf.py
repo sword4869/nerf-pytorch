84e1bea23f9103ed406302ade8547cc713ba826b
sword4869 <1084662708@qq.com>
2023-07-09 09:40:12 +0800

    cuda:0

--- run_nerf.py
+++ run_nerf.py
@@ -14,7 +14,7 @@ from load_deepvoxels import load_dv_data
 from load_blender import load_blender_data
 from load_LINEMOD import load_LINEMOD_data
 
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 np.random.seed(0)
 DEBUG = False
